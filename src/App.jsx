b35ca493df36df282a4990bcf121914e3ec43a9a
GolbanEF <Golban.ef@yandex.ru>
2024-04-22 22:03:51 +0300

    go

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WorldTime from './components/WorldTime/WorldTime';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <WorldTime />
+    </div>
+  );
+}
+
+export default App;
